7862df8f0b1ca22c010030f200351e5e7e735a58
zacca22 <a.zacca@gmail.com>
2023-01-11 16:19:43 -0300

    requisito1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
+const ENDPOINT = ' https://api.mercadolibre.com/sites/MLB/';
 export async function getCategories() {
-  // Implemente aqui
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await request.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(CATEGORY_ID, QUERY) {
+  const request = await fetch(`${ENDPOINT}search?${CATEGORY_ID}&q=?${QUERY}`);
+  const response = await request.json();
+  return response;
 }
 
 export async function getProductById() {
