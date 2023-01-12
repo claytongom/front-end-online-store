e3b128bc7f05b7734a9d317bf2263c804283dc27
Ivory <ivorymailing@protonmail.com>
2023-01-11 23:29:49 -0300

    Changing Home.jsx to Pages folder

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Categories from './Categories';
+import Categories from '../Components/Categories';
 
 function Home() {
   return (
